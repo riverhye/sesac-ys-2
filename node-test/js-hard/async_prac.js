0c5351254c6fcd0fea4612013e2109e0d2c5d2ca
riverhye <uhzigani@gmail.com>
2023-10-14 22:41:32 +0900

    JS class, promise 예제

--- node-test/js-hard/async_prac.js
+++ node-test/js-hard/async_prac.js
@@ -1,41 +1,41 @@
-function call(name) {
-    return new Promise((resolve)=>{
-        setTimeout(()=> {
-            console.log(name);
-            resolve(name);
-        }, 1000)
-    })
-}
+// function call(name) {
+//     return new Promise((resolve)=>{
+//         setTimeout(()=> {
+//             console.log(name);
+//             resolve(name);
+//         }, 1000)
+//     })
+// }
 
-function back() {
-    return new Promise((resolve)=>{
-        setTimeout(()=>{
-            console.log('back');
-            resolve('back');
-        }, 1000)
-    })
-}
+// function back() {
+//     return new Promise((resolve)=>{
+//         setTimeout(()=>{
+//             console.log('back');
+//             resolve('back');
+//         }, 1000)
+//     })
+// }
 
-function hell() {
-    return new Promise ((resolve)=>{
-        setTimeout(() => {
-            resolve('callback hell');
-        }, 1000);
-    })
-}
+// function hell() {
+//     return new Promise ((resolve)=>{
+//         setTimeout(() => {
+//             resolve('callback hell');
+//         }, 1000);
+//     })
+// }
 
 
 // [1] then으로
             // !! then에서 resolve(name) 함수를 받아 매개변수 result로 받아옴 !!
-call('kim').then((result)=>{
-    console.log(result + ' 반가워');
-    return back();
-}).then((result)=>{
-    console.log(result + ' 실행');
-    return hell();
-}).then((result)=>{
-    console.log(result);
-});
+// call('kim').then((result)=>{
+//     console.log(result + ' 반가워');
+//     return back();
+// }).then((result)=>{
+//     console.log(result + ' 실행');
+//     return hell();
+// }).then((result)=>{
+//     console.log(result);
+// });
 
 
 // 메서드 체이닝이 안 된 then..
@@ -53,13 +53,55 @@ call('kim').then((result)=>{
 
 
 // [2] async, await로
-async function exec() {
-    const res1 = await call('kim');
-    console.log(`${res1} 반가워`);
-    const res2 = await back();
-    console.log(`${res2}을 실행했구나`);
-    const res3 = await hell();
-    console.log(`${res3}이 아니야`);
+// async function exec() {
+//     const res1 = await call('kim');
+//     console.log(`${res1} 반가워`);
+//     const res2 = await back();
+//     console.log(`${res2}을 실행했구나`);
+//     const res3 = await hell();
+//     console.log(`${res3}이 아니야`);
+// }
+
+// exec();
+
+
+// async/await 다시 짚어보기
+function add(n1, n2) {
+    return new Promise(function(res){
+        setTimeout(function(){
+            let result = n1 + n2;
+            res(result);
+        });
+    }, 2000);
+}
+
+function mul(n) {
+    return new Promise(function(res){
+        setTimeout(() => {
+            let result = n * 3;
+            res(result);
+        }, 2000);
+    });
+}
+
+function sub(n) {
+    return new Promise(function(res){
+        setTimeout(function(){
+            let result = n - 3;
+            res(result);
+        }, 2000);
+    });
+}
+
+// n초마다 하나씩 동기적으로 발생하려면 모든 요소에 await 설정
+// 함수명과 변수명을 다르게!
+async function exec(){
+    const sum = await add(4, 5);
+    console.log(`1: ${sum}`);
+    const mult = await mul(sum);
+    console.log(`2: ${mult}`);
+    const subb = await sub(mult);
+    console.log(`3: ${subb}`);
 }
 
 exec();
--- node-test/js-hard/class-extends-prac.js
+++ node-test/js-hard/class-extends-prac.js
@@ -1,8 +1,8 @@
-function test() {
-    console.log('test');
-}
+// function test() {
+//     console.log('test');
+// }
 
-console.log(test()); // undefined
+// console.log(test()); // undefined
 // WHY? test 함수는 return값이 없어서 호출한 함수의 내용을 콘솔에 찍지만
 // 함수 자체를 콘솔에 찍어봤자 return할 것이 없어 undefined
 
@@ -16,51 +16,53 @@ class Shape {
     }
 
     getArea() {
-        // return으로 반환하면 console.log
-        // console.log로 해두면 걍 메서드 실행
-        const w = Number(this.width);
-        const h = Number(this.height);
-        return w * h;
+        return Number(this.width) * Number(this.height);
     }
 }
 
 const rec1 = new Shape(3, 4);
 console.log(rec1.getArea());
 
-
 // 1. 직사각형 대각선 길이
 class Rectangle extends Shape {
     constructor(width, height) {
+        // super에서 가져오는 인자들은 한번에 기입 (super 한 번만!)
         super(width, height);
     }
 
     getArea() {
-        super.getArea;
-    }
-
-    math() {
-        return Math.sqrt(super(w)**2 + super(h)**2);
+        return Math.sqrt(this.width**2 + this.height**2);
     }
 }
 
 const rec2 = new Rectangle(3, 4);
-console.log(rec2.math());
+console.log(rec2.getArea());
 
 
 // 2. 삼각형 넓이
 class Triangle extends Shape {
     constructor(width, height) {
-    super(width, height);
+        super(width, height);
     }
 
     getArea() {
-        // 나누기..?
-        `삼각형 넓이는 ${Number(super.getArea() / 2)}`;
+        return this.width * this.height / 2
     }
 }
 
-const tri = new Triangle(3, 4);
-tri.getArea();
-
+const tri = new Triangle (3, 4);
+console.log(tri.getArea());
 
 // 3. 원 넓이
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    getArea() {
+        return 3.14 * (this.radius**2);
+    }
+}
+
+const cir = new Circle(4);
+console.log(cir.getArea());
