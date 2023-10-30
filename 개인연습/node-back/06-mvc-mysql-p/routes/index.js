1d450d57bb6364d4c344e20d21ede65c8c1597e8
riverhye <uhzigani@gmail.com>
2023-10-31 00:42:48 +0900

    MVC mysql study2

--- 개인연습/node-back/06-mvc-mysql-p/routes/index.js
+++ 개인연습/node-back/06-mvc-mysql-p/routes/index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controller/Cuser");
 
+// Main
 router.get("/", controller.home);
+
+// Sign Up
 router.post("/register", controller.newRegister);
 
 module.exports = router;
