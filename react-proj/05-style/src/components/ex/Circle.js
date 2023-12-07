f1b6841c823ae3715a19b685d412c218f94dcca6
riverhye <uhzigani@gmail.com>
2023-12-07 21:08:03 +0900

    react scss prac

--- react-proj/05-style/src/components/ex/Circle.js
+++ react-proj/05-style/src/components/ex/Circle.js
@@ -5,8 +5,8 @@ export default function Circle() {
     <>
       <div className="circles">
         <div className="circle circle1"></div>
-        {/* <div className="circle circle2"></div> */}
-        {/* <div className="circle circle3"></div> */}
+        <div className="circle circle2"></div>
+        <div className="circle circle3"></div>
       </div>
     </>
   );
