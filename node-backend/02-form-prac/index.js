44b822e549f0152249e3a9cd02217e7c9652ea6e
riverhye <uhzigani@gmail.com>
2023-12-19 13:35:33 +0900

    error fix

--- node-backend/02-form-prac/index.js
+++ node-backend/02-form-prac/index.js
@@ -2,27 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-app.set('view engine', 'index.ejs');
+app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', function(req, res){
-    res.render('index.ejs')
-})
-app.get('/get', function(req, res){
-    console.log(req.query);
-    res.send('회원가입 완료');
-})
+app.get('/', function (req, res) {
+  res.render('index.ejs');
+});
+app.get('/get', function (req, res) {
+  console.log(req.query);
+  res.send('회원가입 완료');
+});
 
-app.post('/post', function(req, res){
-    console.log(req.body);
-    res.render('result.ejs', {
-        name: req.body.name,
-        gender: req.body.gender
-    });
-})
+app.post('/post', function (req, res) {
+  console.log(req.body);
+  res.render('result.ejs', {
+    name: req.body.name,
+    gender: req.body.gender,
+  });
+});
 
-app.listen(PORT, function(req, res){
-    console.log(`$Server Open : ${PORT}`);
-})
+app.listen(PORT, function (req, res) {
+  console.log(`$Server Open : ${PORT}`);
+});
